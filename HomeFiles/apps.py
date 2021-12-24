66b0956a362b338ae3b2e8fe6f3bf29a91f89f7d
cyberds <dskbagain@gmail.com>
2021-12-24 16:42:11 +0100

    First Addition

---
+++ HomeFiles/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HomefilesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'HomeFiles'
---
+++ VotingEngine/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VotingengineConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'VotingEngine'
